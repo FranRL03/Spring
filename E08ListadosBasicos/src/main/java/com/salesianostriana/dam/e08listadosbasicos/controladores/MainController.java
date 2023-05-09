56d60883a158f49141f8c7a014e83edf2e449973
Fran Ruiz <fjavierrule@gmail.com>
2023-05-09 12:58:31 +0200

    nuevos ejemplos

---
+++ E08ListadosBasicos/src/main/java/com/salesianostriana/dam/e08listadosbasicos/controladores/MainController.java
@@ -0,0 +1,8 @@
+package com.salesianostriana.dam.e08listadosbasicos.controladores;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+}
---
+++ E09FormulariosEdicion/src/main/java/com/salesianostriana/dam/e09formulariosedicion/servicios/CategoriaService.java
@@ -0,0 +1,42 @@
+package com.salesianostriana.dam.e09formulariosedicion.servicios;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.salesianostriana.dam.e09formulariosedicion.modelo.Categoria;
+import com.salesianostriana.dam.e09formulariosedicion.repositorios.CategoriaRepository;
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repositorio;
+	
+	public List<Categoria> findAll() {
+		return repositorio.findAll();
+	}	
+	
+	public List<Categoria> findDestacadas() {
+		return repositorio.findDestacadas();
+	}
+	
+	public Categoria save(Categoria categoria) {
+		return repositorio.save(categoria);
+	}
+	
+	public Categoria findById(Long id) {
+		return repositorio.findById(id).orElse(null);
+	}
+	
+	public Categoria delete(Categoria categoria) {
+		Categoria result = findById(categoria.getId());
+		repositorio.delete(result);
+		return result;
+	}
+	
+	
+
+}
